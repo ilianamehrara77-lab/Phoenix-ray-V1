a773b7760b6cd92e0a6347738a55da3ce76f8d79
Javid2ray <milad90mi@hotmail.com>
2026-05-15 11:51:22 +0330

    Update install.sh

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,11 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "downloading xray"
-wget -O ${PWD}/xray.zip https://github.com/XTLS/Xray-core/releases/download/v26.3.27/Xray-linux-64.zip
+set -e
 
-echo "installing"
-unzip xray.zip && chmod +x xray
-mv xray /usr/local/bin/xray
+echo "[install] downloading xray..."
 
-rm -rf ${PWD}/*
-echo "installed!"
+wget -O /tmp/xray.zip https://github.com/XTLS/Xray-core/releases/download/v26.3.27/Xray-linux-64.zip
+
+echo "[install] extracting..."
+
+unzip /tmp/xray.zip -d /tmp/xray
+
+chmod +x /tmp/xray/xray
+mv /tmp/xray/xray /usr/local/bin/xray
+
+echo "[install] cleanup..."
+
+rm -rf /tmp/xray /tmp/xray.zip
+
+echo "[install] installed successfully!"
